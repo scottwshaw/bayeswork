7edea78b93eee23eacf564c65f25d082df379408
Scott Shaw <scottwshaw@gmail.com>
2013-11-15 18:10:14 -0500

    exercise 5.7

--- src/bayeswork/core.clj
+++ src/bayeswork/core.clj
@@ -6,12 +6,23 @@
   (:use incanter.stats)
   (:use incanter.charts))
 
-(defn plotbeta [a b]
+(defn plot-ptheta 
+  "Plots dbeta priors"
+  [a b]
   (let [theta (range 0.01 1 0.01)
-        beta (pdf-beta theta :alpha a :beta b)]
-    (xy-plot theta beta)))
+        p-theta (pdf-beta theta :alpha a :beta b)]
+    (xy-plot theta p-theta)))
 
-(defn plotprior [n z]
+(defn pdata
+  "computes the evidence for the model; p(D|M) or p(z,N)"
+  [a b z n]
+  (let [theta (range 0.01 1 0.01)]
+    (/ (beta (+ z a) (+ (- n z) b))
+       (beta a b))))
+
+(defn plot-p-data-given-theta 
+  "plots the likelihood of the data for a range of thetas"
+  [n z]
   (let [theta (range 0.01 1 0.01)
         p-data-given-theta (map * (pow theta z) (pow (minus 1 theta) (- n z)))]
     (xy-plot theta p-data-given-theta)))
--- tmp.clj
+++ tmp.clj
@@ -23,7 +23,38 @@
 (def somearray [[1/2 2 3] [4 5 6]])
 
 (* 2 somearray)
+
+(- 2 1)
+
+(pow somearray 2)
 (trans somearray)
+
+(use 'bayeswork.core :reload-all)
+
+(view (plot-ptheta 1 100))
+
+;; Excercise 5.6
+;; probability given extremely biased prior model (trick coin)
+(pdata 0.5 0.5 15 20)
+;; probability given strongly peaked prior at 0.5 (fair coin)
+(pdata 50 50 15 20)
+
+;; Exercise 5.7
+;; Evidence that one heads flip is biased toward heads
+(pdata 1 100 1 1)
+;; Evidence that one heads flip is biased toward heads
+(pdata 100 1 1 1)
+;; Bayes factor
+(/ (pdata 100 1 1 1) (pdata 1 100 1 1))
+
+(pow (range 0 10 0.1) 2.0)
+
+(minus (range 0.01 1 0.01) 1)
+ 
+(ccmo/-  1)
+
+
+
 
 
 
